5a0b21d4c65246f7d4df617c7a1d600e8a29f806
Marley <mpsvideos.marley@gmail.com>
2024-08-30 11:02:38 -0300

    Finalizado a primeira versão

--- mercado/models.py
+++ mercado/models.py
@@ -17,7 +17,7 @@ class User(db.Model, UserMixin):
     @property
     def formataValor(self):
         if len(str(self.valor)) >= 4: # R$ 400
-            return f"R${str(self.valor)[:-3]}.{str(self.valor)[-3:]}"            
+            return f"R$ {str(self.valor)[:-3]}.{str(self.valor)[-3:]}"            
         else:
             return f"R$ {self.valor}"
 
@@ -32,6 +32,12 @@ class User(db.Model, UserMixin):
     def converte_senha(self, senha_texto_claro):
         return bcrypt.check_password_hash(self.senha, senha_texto_claro)
     
+    def compra_disponivel(self, produto_obj):
+        return self.valor >= produto_obj.preco
+    
+    def venda_disponivel(self, produto_obj):
+        return produto_obj in self.itens
+    
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(length=30), nullable=False, unique=True)
@@ -40,5 +46,22 @@ class Item(db.Model):
     descricao = db.Column(db.String(length=1024), nullable=False, unique=True)
     dono = db.Column(db.Integer, db.ForeignKey('user.id'))
 
+    @property
+    def formataPreco(self):
+        if len(str(self.preco)) >= 4: # R$ 400
+            return f"R$ {str(self.preco)[:-3]}.{str(self.preco)[-3:]}"            
+        else:
+            return f"R$ {self.preco}"
+
     def __repr__(self):
         return f"Item {self.nome}"
+    
+    def compra(self, usuario):
+        self.dono = usuario.id
+        usuario.valor -= self.preco
+        db.session.commit()
+
+    def venda(self, usuario):
+        self.dono = None
+        usuario.valor += self.preco
+        db.session.commit()
--- mercado/routes.py
+++ mercado/routes.py
@@ -1,20 +1,43 @@
 from mercado import app
-from flask import render_template, redirect, url_for, flash
+from flask import render_template, redirect, url_for, flash, request
 from mercado.models import Item, User
-from mercado.forms import CadastroForm, LoginForm, CompraProdutoForm
+from mercado.forms import CadastroForm, LoginForm, CompraProdutoForm, VendaProdutoForm
 from mercado import db
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 
 @app.route('/')
 def page_home():
     return render_template("home.html")
 
-@app.route('/produtos')
+@app.route('/produtos', methods=["GET", "POST"])
 @login_required
 def page_produto():
     compra_form = CompraProdutoForm()
-    itens = Item.query.all()
-    return render_template("produtos.html", itens=itens, compra_form=compra_form)
+    venda_form = VendaProdutoForm()
+    if request.method == "POST":
+        # Compra Produto
+        compra_produto = request.form.get('compra_produto')
+        produto_obj = Item.query.filter_by(nome=compra_produto).first()
+        if produto_obj:
+            if current_user.compra_disponivel(produto_obj):
+                produto_obj.compra(current_user)
+                flash(f"Parabéns! Você comprou o produto {produto_obj.nome}", category="success")
+            else:
+                flash(f"Você não possui saldo suficiente para comprar o produto {produto_obj.nome}", category="danger")
+        # Venda Produto
+        venda_produto = request.form.get('venda_produto')
+        produto_obj_venda = Item.query.filter_by(nome=venda_produto).first()
+        if produto_obj_venda:
+            if current_user.venda_disponivel(produto_obj_venda):
+                produto_obj_venda.venda(current_user)
+                flash(f"Parabéns! Você vendeu o produto {produto_obj_venda.nome}", category="success")
+            else:
+                flash(f"Algo deu errado com a venda do produto {produto_obj_venda.nome}", category="danger") 
+        return redirect(url_for('page_produto'))
+    if request.method =="GET":
+        itens = Item.query.filter_by(dono=None)
+        dono_itens = Item.query.filter_by(dono=current_user.id)
+        return render_template("produtos.html", itens=itens, compra_form=compra_form, dono_itens=dono_itens, venda_form=venda_form)
 
 @app.route('/cadastro', methods=['GET', 'POST'])
 def page_cadastro():
